1a6bdb54d6258891a37481d89e23f571c2ea0ce9
Julia Brown <julia.margaret.brown@gmail.com>
2015-10-19 16:43:00 -0400

    Added to annotation_dev ipynb

---
+++ scripts/split_fasta.py
@@ -0,0 +1,33 @@
+#!usr/bin/python
+
+import sys
+from pyfaidx import Fasta
+import numpy as np
+import re
+
+#code to split a fasta file into the desired number of fasta files:
+fasta_file=sys.argv[1]
+number_files=int(sys.argv[2])
+
+def split_fasta(number_files, fasta_file):
+    fasta=Fasta(fasta_file)
+    number_seqs=len(fasta.keys())
+    splits=int(np.ceil(number_seqs/number_files))
+    #print(splits)
+    ranges=range(0, number_seqs, splits)
+    print(ranges)
+    ranges[-1]=number_seqs
+    print(ranges)
+
+    
+    for i in range(0, number_files):
+        start=ranges[i]
+        stop=ranges[i+1]
+        label=re.sub(r"\.fa.*","."+str(i+1)+".fasta", fasta_file)
+        out=open(label,"w")
+        
+        for f in fasta.keys()[start:stop]:
+            out.write(">"+f+"\n"+str(fasta[f])+"\n")
+        out.close()
+        
+split_fasta(number_files, fasta_file)
